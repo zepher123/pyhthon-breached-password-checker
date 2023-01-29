b4c16451a746df1e0a8dc36fdff87b0145ea4952
Utsav Paudel <utsavpaudel29@gmail.com>
2023-01-29 14:58:56 +0545

    print encoded password

--- password_checker.py
+++ password_checker.py
@@ -1,5 +1,19 @@
 import requests
+import hashlib #sha1 hashing with this library
 
-url = 'https://haveibeenpwned.com/api/v3/range/' + 'CBFDA' #kate anomity
-res = requests.get(url)
-print(res) #200 means good
+
+
+def request_api_data(querry_char):
+    url = 'https://haveibeenpwned.com/api/v3/range/' + querry_char   # kate anomity
+    res = requests.get(url)
+    #print(res)  # 200 means good
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+
+def pwned_api_check(password):
+    #check password if it exist in api response
+    print(password.encode('utf-8'))
+    # shah1password = hashlib.sha1(password.encode('utf-8'))
+
+
+pwned_api_check('123')
