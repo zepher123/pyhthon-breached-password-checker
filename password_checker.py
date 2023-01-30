4f20e796315cfd0856993252237192074adf775b
Utsav Paudel <utsavpaudel29@gmail.com>
2023-01-30 13:14:58 +0545

    sys.exit added

--- password_checker.py
+++ password_checker.py
@@ -40,4 +40,4 @@ def main(args):
     return 'done'
 
 if __name__ == '__main__':
-    main(sys.argv[1: ])
+    sys.exit(main(sys.argv[1: ])) #to exit process and brings to cmd line incase if it doesn't exit
